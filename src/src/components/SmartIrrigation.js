65d0e1397fe53b619e0ca1f96636175e165a51fa
ANKIT1122334455 <nehadhupia077@gmail.com>
2025-10-25 20:33:43 +0530

    Create SmartIrrigation.js

---
+++ src/src/components/SmartIrrigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function SmartIrrigation() {
+  return (
+    <div className="page">
+      <h2>💧 Smart Irrigation</h2>
+      <p>
+        Smart irrigation systems use weather and soil data to water crops only
+        when needed, saving water and energy.
+      </p>
+      <ul>
+        <li>Automated water control</li>
+        <li>Moisture sensor integration</li>
+        <li>Remote irrigation scheduling</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SmartIrrigation;
