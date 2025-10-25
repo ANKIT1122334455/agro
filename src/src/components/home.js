2d81245f07546da5332cce0b9f28d6efff4d02a6
ANKIT1122334455 <nehadhupia077@gmail.com>
2025-10-25 20:31:29 +0530

    Create home.js

---
+++ src/src/components/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section className="home">
+      <h1>Welcome to AgriSmart</h1>
+      <p>
+        Empowering farmers with technology: Precision Farming, Smart Irrigation,
+        Crop & Soil Monitoring, and more.
+      </p>
+      <img
+        src="https://cdn.pixabay.com/photo/2016/11/21/12/59/agriculture-1846358_1280.jpg"
+        alt="Farming"
+        className="hero-img"
+      />
+    </section>
+  );
+}
+
+export default Home;
